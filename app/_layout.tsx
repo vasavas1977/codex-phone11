54a74649c11504466ed1ae65afc68cb7be3331ee
vasavas1977 <vasavas1977@gmail.com>
2026-05-19 17:20:43 +0700

    Remove Reanimated side-effect import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,6 @@ import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import "react-native-reanimated";
 import { Platform } from "react-native";
 import "@/lib/_core/nativewind-pressable";
 import { ThemeProvider } from "@/lib/theme-provider";
