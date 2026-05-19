2314dbe443db805ab9b7d88e244d1574023f97df
vasavas1977 <vasavas1977@gmail.com>
2026-05-19 17:21:32 +0700

    Disable unused Reanimated native autolinking

---
+++ react-native.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  dependencies: {
+    "react-native-reanimated": {
+      platforms: {
+        ios: null,
+        android: null,
+      },
+    },
+    "react-native-worklets": {
+      platforms: {
+        ios: null,
+        android: null,
+      },
+    },
+  },
+};
